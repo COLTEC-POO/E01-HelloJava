f0ab3fdd4d414604574ca4911181e6f664c7a243
leandro-tcd <leandro.deq@hotmail.com>
2021-10-08 16:46:34 -0300

    Correção de valores Q3 Repeticão3

--- src/parte_03/Repeticao3.java
+++ src/parte_03/Repeticao3.java
@@ -9,7 +9,7 @@ public class Repeticao3 {
         byte i;
 
         //Estrutura de repetição para varrer os números entre 1 e 100
-        for(i=1; i<=100; i++){
+        for(i = 2; i <= 99; i++){
             //Estrutura condicional para verificar quais números são multiplos de sete
             if (i % 7 == 0){
                 System.out.println("O número " + i + " é um multiplo de sete");
