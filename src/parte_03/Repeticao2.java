e7eb5dfaf289ef39aadca9b1e456656e07525a3f
Samuel <samuel.pogum@gmail.com>
2021-10-12 11:53:14 -0300

    Finalização do exercício 03: Repetição 2 feito!

---
+++ src/parte_03/Repeticao2.java
@@ -0,0 +1,16 @@
+package parte_03;
+
+public class Repeticao2 {
+    public static void main (String[]args){
+
+        int i;
+        long cont=0;
+
+        for(i=100;i<1000;i++){
+            cont=cont+i;
+        }
+
+        System.out.println(cont);
+
+    }
+}
