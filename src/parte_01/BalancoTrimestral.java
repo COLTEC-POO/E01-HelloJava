2d73f210fd51a824a2601008e2837542c30ae925
Samuel <samuel.pogum@gmail.com>
2021-10-11 22:24:32 -0300

    Finalização do exercício 01: Variáveis e tipos primitivos.

---
+++ src/parte_01/BalancoTrimestral.java
@@ -0,0 +1,15 @@
+package parte_01;
+
+public class BalancoTrimestral {
+    public static void main(String[]args){
+        int gastosJaneiro=15000;
+        int gastosFevereiro=23000;
+        int gastosMarco=17000;
+
+        int gastosTrimestre=gastosJaneiro+gastosFevereiro+gastosMarco;
+        int mediaMensal=gastosTrimestre/3;
+
+        System.out.println("Gastos do trimestre:"+gastosTrimestre);
+        System.out.println("Média dos gastos:"+mediaMensal);
+    }
+}
