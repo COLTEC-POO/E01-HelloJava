f799d65058d0e7cb9cf2db1c662fbaf426fa73e3
vantunes2020956289 <teiacoltec.org>
2021-10-13 17:59:19 -0300

    Finalização do exercicio 1 :) - Tipos Primitivos e Variáveis (formatação)

--- src/parte_01/BalancoTrimestral.java
+++ src/parte_01/BalancoTrimestral.java
@@ -1,15 +1,17 @@
-package parte_01;
+    package parte_01;
 
-public class BalancoTrimestral {
-    public static void main (String[] args){
-        int gastosJaneiro = 15000;
-        int gastosFevereiro = 23000;
-        int gastosMarco = 17000;
-        int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
-        System.out.println(gastosTrimestre);
+    public class BalancoTrimestral {
 
-        int mediaMensal = (gastosJaneiro + gastosFevereiro + gastosMarco)/3;
-        System.out.println("Valor da media mensal = " + mediaMensal);
+        public static void main (String[] args){
+            int gastosJaneiro = 15000;
+            int gastosFevereiro = 23000;
+            int gastosMarco = 17000;
 
+            int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+            System.out.println(gastosTrimestre);
+
+            int mediaMensal = (gastosJaneiro + gastosFevereiro + gastosMarco)/3;
+            System.out.println("Valor da media mensal = " + mediaMensal);
+
+        }
     }
-}
