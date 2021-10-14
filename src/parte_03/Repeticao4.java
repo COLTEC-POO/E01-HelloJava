8785d5c6ae36e8272e115648ad38e9ced3a6c639
BrendaSales <brendasales1997@gmail.com>
2021-10-14 00:22:43 -0300

    Finalização do exercício 03: Repeticao4

--- src/parte_03/Repeticao4.java
+++ src/parte_03/Repeticao4.java
@@ -1,4 +1,15 @@
 package parte_03;
 
 public class Repeticao4 {
+    public static void main(String[] args) {
+        int i;
+        int fatorial = 1;
+
+        for (i = 10; i > 0; i--) {
+            fatorial = fatorial * i;
+        }
+        System.out.println("O fatorial do numero 10 é: " + fatorial);
+
+    }
 }
+
