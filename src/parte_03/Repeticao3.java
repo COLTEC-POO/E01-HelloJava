90802248150acaecdc0be33b93b24c25131362b0
BrendaSales <brendasales1997@gmail.com>
2021-10-13 23:49:47 -0300

    Finalização do exercício 03: Repeticao3

---
+++ src/parte_03/Repeticao3.java
@@ -0,0 +1,15 @@
+package parte_03;
+
+public class Repeticao3 {
+    public static void main(String[] args) {
+        int i;
+        int soma = 0;
+
+        for (i = 1; i <=100; i++){
+            if (i % 7 == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
