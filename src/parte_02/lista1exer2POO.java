bece0c66c9f947c36a10242ed7c47489b68c9502
13RodriguesBruna <107412241+13RodriguesBruna@users.noreply.github.com>
2022-09-08 22:22:14 -0300

    Add files via upload

---
+++ src/parte_02/lista1exer2POO.java
@@ -0,0 +1,24 @@
+//Repeticao2.java: Imprima a soma entre 100 e 1000
+ 
+import java.util.Scanner;
+
+public class Condicional {
+     public static void main(String[] args){
+
+          Sacannser estrada = new Scanner(System.in)
+
+          int valor;
+
+        System.outprintln(“Digite o valor:”);
+         valor = entrada.nextInt();
+
+        if(valor) % 2 == 0)
+                  
+              System.out.println(“ Valor é par”);
+
+      else
+                System.out.println(“ Valor é impar”);
+
+
+}
+}
