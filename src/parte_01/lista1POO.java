f6091115716f04bd69c15ad9b6b6e6483122adad
13RodriguesBruna <107412241+13RodriguesBruna@users.noreply.github.com>
2022-09-08 22:21:38 -0300

    Add files via upload

---
+++ src/parte_01/lista1POO.java
@@ -0,0 +1,17 @@
+//Repeticao1.java: Imprima todos os números entre 100 e 1000
+ 
+
+public static void main(String) args){
+   int gastosJaneiro =15000;
+   int gastosFevereiro =23000;
+   int gastoMarco =17000
+
+int gastoTrimestre = gastoJaneiro + gastosFevereiro + gastoMarco;
+int mediaMensal = gastoTrimestre /3
+}
+
+System.out. println(“Gastos trimestre:” + gastoTrimestre);
+System.out.println(“Media dos gastos:” + mediaMensal);
+
+}
+} 
