f9ee4b194ed20d418c2ca1d904b9cb71336eeb6b
Pedro Nunes <pedrohvn4@gmail.com>
2022-09-09 04:02:07 -0300

    Exercício 03: Estruturas de Repetição

---
+++ src/parte_03/Repeticao1.java
@@ -0,0 +1,10 @@
+package parte_03;
+
+public class Repeticao1 {
+    public static void main(String[] args) {
+
+        for (int i = 100; i <= 1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
