0cc87c498be5edf2d58dedd1112422e5c3156a82
maíra <mairaaml11@gmail.com>
2022-09-08 23:29:25 -0300

    Exercício 02: Condicional3 feito.

---
+++ src/parte_02/Condicional3.java
@@ -0,0 +1,21 @@
+package parte_02;
+
+import java.util.Scanner;
+
+public class Condicional3 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Insira a nota para verificar a situação do aluno: ");
+        nota = entrada.nextInt();
+
+        if (nota >= 60){
+            System.out.println("Aprovado! :)");
+        } else {
+            System.out.println("Reprovado! :,(");
+        }
+    }
+}
