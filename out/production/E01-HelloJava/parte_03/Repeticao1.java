995846c4eaea5ed6200a94d5541f77928cc87a81
Marina Villaca <marinavillaca7@gmail.com>
2023-08-25 15:47:14 -0300

    Exercício 03: Estruturas de Repetição Repeticao1.java

---
+++ out/production/E01-HelloJava/parte_03/Repeticao1.java
@@ -0,0 +1,25 @@
+/*
+ * UFMG - Universidade Federal de Minas Gerais
+ * COLTEC - Colégio Técnico
+ * Disciplina: Programação Orientada a Objetos
+ * Prof. João Eduardo
+ *
+ * Aluna: Marina Laura Villaça e Melo
+ * Matrícula: 2023953906
+ */
+
+//     Exercício 03: Estruturas de Repetição
+//     Repeticao1.java: Imprima todos os números entre 100 e 1000
+
+package parte_03;
+import java.util.Scanner;
+
+public class Repeticao1 {
+
+    public static void main(String[] args) {
+
+        for(int i=100; i<=1000; i++){
+            System.out.println(i);
+        }
+    }
+}
