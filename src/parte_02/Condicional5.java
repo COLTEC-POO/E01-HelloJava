e4b7fdadce9b3d351f5a45dc5310ee6ed0dd3bb5
mairamml <mairaaml11@gmail.com>
2023-08-28 15:43:00 -0300

    Exercício 02: Condicional5

---
+++ src/parte_02/Condicional5.java
@@ -0,0 +1,29 @@
+package parte_02;
+
+import java.util.Scanner;
+
+public class Condicional5 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int num1, num2;
+
+        System.out.println("Qual número é maior?");
+
+        System.out.println("Digite o primeiro número: ");
+        num1 = entrada.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        num2 = entrada.nextInt();
+
+        if (num1 > num2){
+            System.out.println(num1 + " é maior!");
+        } else if (num2 > num1) {
+            System.out.println(num2 + " é maior!");
+        }else {
+            System.out.println("Os números são iguais.");
+        }
+
+    }
+}
