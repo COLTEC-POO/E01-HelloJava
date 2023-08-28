0e1933c4fc8b525a510da1fd14f7429483ed870f
mairamml <mairaaml11@gmail.com>
2023-08-28 15:39:19 -0300

    Exercício 02: Condicional4

---
+++ src/parte_02/Condicional4.java
@@ -0,0 +1,29 @@
+package parte_02;
+
+import java.util.Scanner;
+
+public class Condicional4 {
+    public static void main(String[] args){
+
+        Scanner entrada = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Digite a nota para descobrir seu conceito: ");
+        nota = entrada.nextInt();
+
+        if (nota >= 90){
+            System.out.println("Conceito A");
+        } else if (nota >= 80){
+            System.out.println("Conceito B");
+        } else if (nota >= 70) {
+            System.out.println("Conceito C");
+        } else if (nota >= 60) {
+            System.out.println("Conceito D");
+        } else if (nota >= 40) {
+            System.out.println("Conceito E");
+        }else{
+            System.out.println("Conceito F");
+        }
+    }
+}
