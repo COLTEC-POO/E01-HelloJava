be1573025f630d56a15ccc3cc0a354e65a085418
mairamml <mairaaml11@gmail.com>
2023-08-28 15:56:45 -0300

    Exercício 03: Repeticao2.

---
+++ src/parte_03/Repeticao2.java
@@ -0,0 +1,14 @@
+package parte_03;
+
+public class Repeticao2 {
+    public static void main(String[] args){
+
+        int i, soma = 0;
+
+        for(i = 100; i <= 1000; i++){
+            soma += i;
+        }
+
+        System.out.println("Soma = " + soma);
+    }
+}
