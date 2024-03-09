e06dcfa8470c6960f9908f3b04d3f06860093494
icaro2223 <144714161+icaro2223@users.noreply.github.com>
2024-03-09 11:35:10 -0300

    Create Repeticao2.java

---
+++ src/parte_03/Repeticao2.java
@@ -0,0 +1,12 @@
+public class Repeticao2{
+    
+	public static void main(String[] args) {
+	    int soma = 0;
+	    int i;
+	    for(i = 100; i <= 1000; i++){
+	        soma = soma + i;
+	        System.out.println(soma);
+	    }
+	     
+	}
+}
