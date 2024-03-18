a956aef1873652531464422871d2f7952f73ebba
HeitorSF07 <a2023952969@teiacoltec.org>
2024-03-18 16:28:41 -0300

    Implementação da primeira questão da parte 2

---
+++ src/parte_02/Condicional1.java
@@ -0,0 +1,14 @@
+package parte_02;
+import java.util.Scanner;
+public class Condicional1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número:");
+        int numero = scanner.nextInt();
+        if(numero%2==0){
+            System.out.println("O número é par");
+        }else{
+            System.out.println("O número é ímpar");
+        }
+    }
+}
