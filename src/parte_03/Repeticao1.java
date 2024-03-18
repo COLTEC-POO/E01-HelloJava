b4a575ff37917d77734479ca5a60f247613e7750
a2023952586 <a2023952586@teiacoltec.org>
2024-03-18 16:28:37 -0300

    Completo

--- src/parte_03/Repeticao1.java
+++ src/parte_03/Repeticao1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Repeticao1 {
     public static void main(String[] args){
-        for(int i = 100; i<=1000; ++i){
+        for(int i = 100; i<=10000; ++i){
             System.out.print(i+" ");
             if(i%10 ==0){
                 System.out.println();
