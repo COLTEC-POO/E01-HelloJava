42c8b2377a7c0e53853b6a24b5f18d4dfe737404
HeitorSF07 <a2023952969@teiacoltec.org>
2024-03-18 16:49:28 -0300

    Implementação da segundo questão da parte 3

---
+++ src/parte_03/Repeticao2.java
@@ -0,0 +1,11 @@
+package parte_03;
+
+public class Repeticao2 {
+    public static void main(String[] args){
+        int result=100;
+        for(int i=101; i<1001; i++){
+            result = result+i;
+        }
+        System.out.println(result);
+    }
+}
