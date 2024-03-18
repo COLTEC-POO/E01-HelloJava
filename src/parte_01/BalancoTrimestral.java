4901267b55a0180b95a526680e16f0683d861722
Duda000 <mariaeduardateixeira0308@gmail.com>
2024-03-18 16:23:12 -0300

    Implementação da parte 1.

--- src/parte_01/BalancoTrimestral.java
+++ src/parte_01/BalancoTrimestral.java
@@ -5,6 +5,8 @@ public class BalancoTrimestral {
         public static void main(String args[]) {
             int gastosJaneiro = 15000;
             int gastosFevereiro = 17000;
+
+
             int gastosMarco = 23000;
             int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
             int mediaMensal = gastosTrimestre / 3;
