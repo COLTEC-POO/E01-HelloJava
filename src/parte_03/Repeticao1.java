b73199665575c5af89f0ed6d5717ea87589f5216
anajufg <anajufg127@gmail.com>
2024-03-18 16:54:50 -0300

    Implementação da pate 3.

---
+++ src/parte_03/Repeticao1.java
@@ -0,0 +1,15 @@
+package parte_03;
+
+public class Repeticao1 {
+
+    public static void main(String[] args) {
+        int num = 100;
+
+        System.out.println("Números entre 100 e 1000");
+
+        for (int i = 0; i <= 900; i++) {
+            System.out.println(+ num);
+            num++;
+        }
+    }
+}
