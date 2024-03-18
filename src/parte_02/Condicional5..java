edf039d595fd298fb304ab74c4c76941390700b8
a2023951482 <samugonlob12@gamil.com>
2024-03-18 16:58:42 -0300

    Exercicio2

--- src/parte_02/Condicional5..java
+++ src/parte_02/Condicional5..java
@@ -10,6 +10,7 @@ class Condicional5{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         print("Digite o primeiro numero : ");
+
         int a = sc.nextInt();
         print("Digite o segundo numero : ");
         int b = sc.nextInt();
