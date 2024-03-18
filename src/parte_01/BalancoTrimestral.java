dbefb8c75a30ca2870191d62b209f2ea895ad2b5
Walter <walternet2007k@gmail.com>
2024-03-18 16:23:02 -0300

    Pasta1

---
+++ src/parte_01/BalancoTrimestral.java
@@ -0,0 +1,20 @@
+package parte_01;
+
+public class BalancoTrimestral {
+
+    public static void main(String[] args){
+
+        //Criando e inicializando as variáveis int de gastos do mês
+        int gastosJaneiro = 15000;
+        int gastosFevereiro = 23000;
+        int gastosMarco = 17000;
+        //Criando e inicializando a variável de gastos total do Trimestre(soma dos 3 meses)
+        int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+        //Imprimindo os gastos do trimestre
+        System.out.printf("Gastos no trimestre: %d\n", gastosTrimestre);
+        System.out.print("Valor da media mensal: " + (gastosTrimestre / 3) + "\n");
+
+    }
+
+
+}
