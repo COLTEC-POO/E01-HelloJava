c27f6e209ca29bc50939f35494aa68b925e81ad1
Duda000 <mariaeduardateixeira0308@gmail.com>
2024-03-18 16:55:20 -0300

    Implementação da parte 3.

---
+++ src/parte_03/Repeticao2.java
@@ -0,0 +1,11 @@
+package parte_03;
+
+public class Repeticao2 {
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int i = 100; i <= 1000; i++) {
+            soma += i;
+        }
+        System.out.println("A soma dos números de 100 a 1000 é: " + soma);
+    }
+}
