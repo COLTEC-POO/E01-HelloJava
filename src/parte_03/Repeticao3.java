b464bd899d3dfdef997e999c3e863032387dcbf2
SimonNPC <anabeatriz.andrade015@gmail.com>
2024-03-19 13:40:49 -0300

    Implementação da terceira parte

---
+++ src/parte_03/Repeticao3.java
@@ -0,0 +1,15 @@
+package parte_03;
+
+public class Repeticao3 {
+
+    public static void main(String[] args) {
+        // Loop de 1 a 100
+        for (int i = 1; i <= 100; i++) {
+            // Verifica se o número é um múltiplo de 7
+            if (i % 7 == 0) {
+                System.out.println(i); // Imprime o número
+            }
+        }
+    }
+
+}
